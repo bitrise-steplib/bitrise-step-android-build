fe475b3da876a88e0f7a7db1bff6106533f96058
Olivér Falvai <oliver.falvai@bitrise.io>
2022-07-14 18:28:34 +0200

    Fix relative project paths (#45)
    * Fix relative project paths
    
    * Fix Gradle command stderr

--- step/step.go
+++ step/step.go
@@ -310,9 +310,13 @@ func (a AndroidBuild) executeGradleBuild(cfg Config) error {
 	cmdOpts := command.Opts{
 		Dir:    cfg.ProjectLocation,
 		Stdout: os.Stdout,
-		Stderr: os.Stdin,
+		Stderr: os.Stderr,
 	}
-	cmd := a.cmdFactory.Create(filepath.Join(cfg.ProjectLocation, "gradlew"), cmdArgs, &cmdOpts)
+	absPath, err := filepath.Abs(cfg.ProjectLocation)
+	if err != nil {
+		return err
+	}
+	cmd := a.cmdFactory.Create(filepath.Join(absPath, "gradlew"), cmdArgs, &cmdOpts)
 
 	a.logger.Println()
 	a.logger.Donef("$ " + cmd.PrintableCommandArgs())
